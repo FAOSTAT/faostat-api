b1a5081e6fe274ae6e619b5c5f8b017fbb791b7b
Kalimaha <guido.barbaglia@gmail.com>
2015-08-20 16:20:32 +0200

    Templates.

--- src/js/application.js
+++ src/js/application.js
@@ -19,6 +19,12 @@ define(['jquery',
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
         /* Register partials. */
+        Handlebars.registerPartial('menu', $(templates).filter('#menu').html());
+        Handlebars.registerPartial('introduction', $(templates).filter('#introduction').html());
+        Handlebars.registerPartial('introduction_languages', $(templates).filter('#introduction_languages').html());
+        Handlebars.registerPartial('table_headers', $(templates).filter('#table_headers').html());
+        Handlebars.registerPartial('table_commons', $(templates).filter('#table_commons').html());
+        Handlebars.registerPartial('home', $(templates).filter('#home').html());
         Handlebars.registerPartial('home_groups', $(templates).filter('#home_groups').html());
 
         /* Load template. */
