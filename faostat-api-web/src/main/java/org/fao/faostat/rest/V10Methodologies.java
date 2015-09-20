fe8c6937534910e314e9eecdb005feaeeb0cbb03
Kalimaha <guido.barbaglia@gmail.com>
2015-09-20 16:07:57 +0200

    Methodologies service.

---
+++ faostat-api-web/src/main/java/org/fao/faostat/rest/V10Methodologies.java
@@ -0,0 +1,51 @@
+package org.fao.faostat.rest;
+
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+/**
+ * @author <a href="mailto:guido.barbaglia@gmail.com">Guido Barbaglia</a>
+ * */
+@Component
+@Path("/v1.0/{lang}/methodologies/")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class V10Methodologies extends V10 {
+
+    public V10Methodologies() {
+        super();
+    }
+
+    @GET
+    public Response getGroups(@PathParam("lang") String lang,
+                              @QueryParam("datasource") String datasource,
+                              @QueryParam("api_key") String api_key,
+                              @QueryParam("client_key") String client_key,
+                              @QueryParam("output_type") String output_type) {
+
+
+        /* Store user preferences. */
+        this.storeUserOptions(datasource, lang, api_key, client_key, output_type);
+
+        /* Store procedure parameters. */
+        this.getO().addParameter("lang", this.faostatapiCore.iso2faostat(lang));
+
+        /* Query the DB and return the results. */
+        try {
+
+            /* Query the DB and create an output stream. */
+            StreamingOutput stream = this.getFaostatapiCore().createOutputStream("methodologies", this.getO());
+
+            /* Stream result */
+            return Response.status(200).entity(stream).build();
+
+        } catch (Exception e) {
+            return Response.status(500).entity(e.getMessage()).build();
+        }
+
+    }
+
+}
